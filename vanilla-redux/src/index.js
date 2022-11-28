70ef4ec0644c110bd7a013fae14a2f0845e898e2
soprue <soprue@naver.com>
2022-11-29 01:02:19 +0900

    refact : redux 활용하여 코드 리팩토링
    - index.js에서 바닐라 자바스크립트로 기능 구현 하던 걸 리덕스를 활용하여 수정함
    - ---
    - Store: data를 저장하는 곳
    - CreateStore()는 인수로reducer(함수)를 요구함
    - Reducer는 data를 modify 해 주는 함수로 reducer가 return 하는 것은 application에 있는 data가 됨
    - Action: redux에서 function을 부를 때 쓰는 두 번째 parameter 혹은 argument으로 reducer와 소통하기 위한 방법
    - ---
    - getState(): store에 있는 데이터 불러오기
    - dispatch(): Reducer에게 Action을 보내는 방법 => store.dispatch({ type: action_name });
    - Subscribe(): store 안에 있는 변화 감지 => store.subscribe(func); // store 안의 변화를 감지하면 func 실행

--- vanilla-redux/src/index.js
+++ vanilla-redux/src/index.js
@@ -1,23 +1,30 @@
+import { legacy_createStore as createStore } from 'redux';
+
 const add = document.getElementById("add");
 const minus = document.getElementById("minus");
 const number = document.querySelector("span");
+number.innerText = 0;
 
-let count = 0;
-number.innerText = count;
+const ADD = "ADD";
+const MINUS = "MINUS";
 
-const updateText = () => {
-  number.innerText = count;
+const countModifier = (count = 0, action) => {
+  switch (action.type) {
+    case ADD:
+      return count + 1;
+    case MINUS:
+      return count - 1;
+    default:
+      return count;
+  } 
+};
+
+const countStore = createStore(countModifier);
+
+const onChange = () => {
+  number.innerText = countStore.getState();
 }
+countStore.subscribe(onChange);
 
-const handleAdd = () => {
-  count++;
-  updateText();
-}
-
-const handleMinus = () => {
-  count--;
-  updateText();
-}
-
-add.addEventListener("click", handleAdd);
-minus.addEventListener("click", handleMinus);
+add.addEventListener("click", () => countStore.dispatch({ type: ADD }));
+minus.addEventListener("click", () => countStore.dispatch({ type: MINUS }));
