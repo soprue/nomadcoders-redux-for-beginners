60a796c8b7f75c724263ad06b9ce893e20d9b499
soprue <soprue@naver.com>
2022-12-03 17:44:37 +0900

    feat : DELETE_TODO 기능 구현

--- vanilla-redux-todo/src/index.js
+++ vanilla-redux-todo/src/index.js
@@ -10,29 +10,41 @@ const DELETE_TODO = "DELETE_TODO";
 const reducer = (state = [], action) => {
   switch (action.type) {
     case ADD_TODO:
-      return [...state, { text: action.text, id: action.id }];
+      return [ { text: action.text, id: action.id }, ...state ];
     case DELETE_TODO:
-      return [];
+      return state.filter(toDo => toDo.id !== action.id);
     default:
       return state;
   }
 }
 const store = createStore(reducer);
 
-const paintToDos = () => {
-  const toDos = store.getState();
-  toDos.forEach(toDo => {
-    const li = document.createElement("li");
-    li.id = toDo.id;
-    li.innerText = toDo.text;
-    ul.appendChild(li);
-  });
-}
-store.subscribe(paintToDos);
-
 const addToDo = (text) => {
   store.dispatch({ type: ADD_TODO, text, id: Date.now() });
 }
+
+const deleteToDo = (e) => {
+  const id = parseInt(e.target.parentNode.id);
+  store.dispatch({ type: DELETE_TODO, id });
+}
+
+const paintToDos = () => {
+  const toDos = store.getState();
+  ul.innerHTML = "";
+  toDos.forEach(toDo => {
+    const li = document.createElement("li");
+    const btn = document.createElement("button");
+    btn.innerText = "DEL";
+    btn.addEventListener("click", deleteToDo);
+    li.id = toDo.id;
+    li.innerText = toDo.text;
+    li.appendChild(btn);
+    ul.appendChild(li);
+  });
+}
+
+store.subscribe(paintToDos);
+
 const onSubmit = e => {
   e.preventDefault();
   const toDo = input.value;
